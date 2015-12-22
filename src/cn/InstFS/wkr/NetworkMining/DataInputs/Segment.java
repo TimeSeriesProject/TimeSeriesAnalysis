b3a833860eff4e8f55699ce5886a332800fc35d1
chenwei <277351771@qq.com>
2015-12-22 14:45:00 +0800

    聚类拆分

---
+++ src/cn/InstFS/wkr/NetworkMining/DataInputs/Segment.java
@@ -0,0 +1,41 @@
+package cn.InstFS.wkr.NetworkMining.DataInputs;
+
+public class Segment {
+
+	
+	public double getCentery() {
+		return centery;
+	}
+	public void setCentery(double centery) {
+		this.centery = centery;
+	}
+	public double getSlope() {
+		return slope;
+	}
+	public void setSlope(double slope) {
+		this.slope = slope;
+	}
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	double centery;
+	double  slope;
+	double length;
+	Segment(double centery,double slope,double length)
+	{
+		this.centery = centery;
+		this.slope   = slope;
+		this.length  = length;
+	}
+	Segment()
+	{}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
