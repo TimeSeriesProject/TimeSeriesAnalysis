2fc3ce2d6faa621e90295e51d0f57c7b95d5bb21
xunmi67 <zhjx@hust.edu.cn>
2016-04-11 15:13:39 +0800

    add line pattern

---
+++ src/cn/InstFS/wkr/NetworkMining/DataInputs/Pattern.java
@@ -0,0 +1,40 @@
+package cn.InstFS.wkr.NetworkMining.DataInputs;
+
+public class Pattern {
+	private int start;
+	private int end;
+	private double span;
+	public double getSpan() {
+		return span;
+	}
+	public void setSpan(double span) {
+		this.span = span;
+	}
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	private double slope;
+	//private double length;
+	private double average;
+	public double getAverage() {
+		return average;
+	}
+	public void setAverage(double average) {
+		this.average = average;
+	}
+	public double getSlope() {
+		return slope;
+	}
+	public void setSlope(double slope) {
+		this.slope = slope;
+	}
+}
