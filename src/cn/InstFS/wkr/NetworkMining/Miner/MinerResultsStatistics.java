3ad8a10866e3ac19b9a1693db7dbb5fcbfcb404f
xunmi67 <zhjx@hust.edu.cn>
2016-05-17 21:06:16 +0800

    add statistics results

---
+++ src/cn/InstFS/wkr/NetworkMining/Miner/MinerResultsStatistics.java
@@ -0,0 +1,53 @@
+package cn.InstFS.wkr.NetworkMining.Miner;
+
+public class MinerResultsStatistics {
+	private double mean;
+	private double std;
+	private double complex;
+	private double sampleENtropy;
+	private int span;
+	
+	
+	public MinerResultsStatistics(){}
+	
+	public MinerResultsStatistics(double mean, double std, double complex,
+			double sampleENtropy, int span) {
+		this.mean = mean;
+		this.std = std;
+		this.complex = complex;
+		this.sampleENtropy = sampleENtropy;
+		this.span = span;
+	}
+	public double getMean() {
+		return mean;
+	}
+	public void setMean(double mean) {
+		this.mean = mean;
+	}
+	public double getStd() {
+		return std;
+	}
+	public void setStd(double std) {
+		this.std = std;
+	}
+	public double getComplex() {
+		return complex;
+	}
+	public void setComplex(double complex) {
+		this.complex = complex;
+	}
+	public double getSampleENtropy() {
+		return sampleENtropy;
+	}
+	public void setSampleENtropy(double sampleENtropy) {
+		this.sampleENtropy = sampleENtropy;
+	}
+	public int getSpan() {
+		return span;
+	}
+	public void setSpan(int span) {
+		this.span = span;
+	}
+	
+	
+}
