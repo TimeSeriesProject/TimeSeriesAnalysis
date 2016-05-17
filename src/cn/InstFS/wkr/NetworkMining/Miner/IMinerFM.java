fd59f9ee4524b0ff2ad1005861d70b1c6ac86613
xunmi67 <zhjx@hust.edu.cn>
2016-05-17 10:38:50 +0800

    add FM

---
+++ src/cn/InstFS/wkr/NetworkMining/Miner/IMinerFM.java
@@ -0,0 +1,8 @@
+package cn.InstFS.wkr.NetworkMining.Miner;
+
+import cn.InstFS.wkr.NetworkMining.DataInputs.DataItems;
+
+public interface IMinerFM {
+	public void TimeSeriesAnalysis();
+	public DataItems getPredictItems();
+}
