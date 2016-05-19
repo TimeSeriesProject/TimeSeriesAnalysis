6e0ff2422e823f56a8cd7780c58ef38063991758
836304831 <836304831@qq.com>
2016-05-19 10:16:19 +0800

    update

--- src/associationRules/ProtocolAssociationResult.java
+++ src/associationRules/ProtocolAssociationResult.java
@@ -1,5 +1,8 @@
 package associationRules;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.InstFS.wkr.NetworkMining.DataInputs.DataItems;
 
 /**
@@ -13,8 +16,22 @@ public class ProtocolAssociationResult{
 	int bias = 0;
 	String protocol1 = "";
 	String protocol2 = "";
-	DataItems dataItems1;
-	DataItems dataItems2;
+	public DataItems dataItems1;
+	public DataItems dataItems2;
+	
+	public List<String> assA1 = new ArrayList<String>();
+	public List<String> assA2 = new ArrayList<String>();
+	
+	public List<String> assB1 = new ArrayList<String>();
+	public List<String> assB2 = new ArrayList<String>();
+	public ProtocolAssociationResult(String p1,String p2,DataItems data1,DataItems data2){
+		
+		protocol1 = p1;
+		protocol2 = p2;
+		dataItems1 = data1;
+		dataItems2 = data2;
+	}
+	
 	public ProtocolAssociationResult(String p1,String p2,DataItems data1,DataItems data2,double s,int k)
 	{
 		protocol1 = p1;
