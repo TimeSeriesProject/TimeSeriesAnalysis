369e31e7b237c2da43d0929999872e1e5aa0dc5c
836304831 <836304831@qq.com>
2016-05-19 10:59:02 +0800

    add

---
+++ src/associationRules/ProtocolAssociationResultLine.java
@@ -0,0 +1,20 @@
+package associationRules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.InstFS.wkr.NetworkMining.DataInputs.DataItems;
+
+public class ProtocolAssociationResultLine extends ProtocolAssociationResult{
+
+	public List<String> assA = new ArrayList<String>();
+	public List<String> assB = new ArrayList<String>();
+	public ProtocolAssociationResultLine(String p1,String p2,DataItems data1,DataItems data2){
+		
+		protocol1 = p1;
+		protocol2 = p2;
+		dataItems1 = data1;
+		dataItems2 = data2;
+		alogrithmType = 1;
+	}
+}
