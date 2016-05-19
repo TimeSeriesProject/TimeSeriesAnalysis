9d7feb7233320c9c3c33d357fd14815e14ba2ae9
836304831 <836304831@qq.com>
2016-05-19 10:58:37 +0800

    add

---
+++ src/associationRules/ProtocolAssociationResultRate.java
@@ -0,0 +1,43 @@
+package associationRules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.InstFS.wkr.NetworkMining.DataInputs.DataItems;
+
+/**
+ * 存储协议关联规则挖掘的结果
+ * @author Administrator
+ *
+ */
+public class ProtocolAssociationResultRate extends ProtocolAssociationResult{
+	
+	double support = 0.0;
+	int bias = 0;
+	
+	
+	public List<String> assA1 = new ArrayList<String>();
+	public List<String> assA2 = new ArrayList<String>();
+	
+//	public List<String> assB1 = new ArrayList<String>();
+//	public List<String> assB2 = new ArrayList<String>();
+	public ProtocolAssociationResultRate(String p1,String p2,DataItems data1,DataItems data2){
+		
+		protocol1 = p1;
+		protocol2 = p2;
+		dataItems1 = data1;
+		dataItems2 = data2;
+		alogrithmType = 0;
+	}
+	
+	public ProtocolAssociationResultRate(String p1,String p2,DataItems data1,DataItems data2,double s,int k)
+	{
+		protocol1 = p1;
+		protocol2 = p2;
+		dataItems1 = data1;
+		dataItems2 = data2;
+		support = s;
+		bias = k;
+	}
+	
+}
