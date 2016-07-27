b7a223bddd58a50f005ca1c7a1d041843aead433
LYH <LYH@LYH-PC>
2016-07-27 19:23:19 +0800

    承载路径挖掘周期显示界面

--- src/cn/InstFS/wkr/NetworkMining/ResultDisplay/UI/PanelShowResultsPP.java
+++ src/cn/InstFS/wkr/NetworkMining/ResultDisplay/UI/PanelShowResultsPP.java
@@ -1,18 +1,24 @@
 package cn.InstFS.wkr.NetworkMining.ResultDisplay.UI;
 
+import WaveletUtil.PointPatternDetection;
 import cn.InstFS.wkr.NetworkMining.DataInputs.DataItem;
 import cn.InstFS.wkr.NetworkMining.DataInputs.DataItems;
+import cn.InstFS.wkr.NetworkMining.DataInputs.DataPretreatment;
 import cn.InstFS.wkr.NetworkMining.Miner.*;
+import cn.InstFS.wkr.NetworkMining.TaskConfigure.MiningAlgo;
 import cn.InstFS.wkr.NetworkMining.TaskConfigure.MiningMethod;
 import cn.InstFS.wkr.NetworkMining.TaskConfigure.TaskElement;
+
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.awt.geom.Arc2D;
 import java.util.*;
+import java.util.List;
 
 public class PanelShowResultsPP extends JPanel implements IPanelShowResults {
 
@@ -20,19 +26,23 @@ public class PanelShowResultsPP extends JPanel implements IPanelShowResults {
     private MiningMethod miningMethod;
     ChartPanelShowTs chart1;
     int count=0;
+    TaskElement task;
+    List<TaskCombination> alltaskCombination;
+    TaskCombination taskCombination;
 
     /**
      * Create the panel.
      */
     public PanelShowResultsPP(TaskElement task) {
-
+    	this.task = task;
+    	alltaskCombination = task.allCombinationTasks;    	
         setLayout(new GridLayout(0, 1, 0, 0));
         chart1 = new ChartPanelShowTs("路径挖掘", "时间", "值", null);
         add(chart1);
         miningMethod = task.getMiningMethod();
         InitMiner(task);
     }
-
+    
     private void InitMiner(TaskElement task) {
         this.miner = NetworkMinerFactory.getInstance().createMiner(task);
         miner.setResultsDisplayer(this);
@@ -273,6 +283,8 @@ public class PanelShowResultsPP extends JPanel implements IPanelShowResults {
             HashMap<String,Integer> period = new HashMap<>();
             HashMap<String,Integer> firstPeriod = new HashMap<>();
             HashMap<String,DataItems> pathDataItems = new HashMap<>();
+            
+            
 
             for (Map.Entry<String, MinerResultsPM> entry : resultPM.entrySet()) {
                 String pathname = entry.getKey();
@@ -311,8 +323,8 @@ public class PanelShowResultsPP extends JPanel implements IPanelShowResults {
                 sortPeriod.put(min,periodName);
                 min=1000000000;
             }
-
-            jp.setLayout(new GridLayout(0,2));
+            
+            jp.setLayout(new GridLayout(0,1));
             jsp.setViewportView(jp);
             add(jsp);
             /**帮助解读:
@@ -329,25 +341,50 @@ public class PanelShowResultsPP extends JPanel implements IPanelShowResults {
                 for(int i=0;i<oneSeries.size();i++)
                 {                    
                 	String tempName=oneSeries.get(i);
+                	
+                	/*从tempName中获取源和目的IP*/
+                	String[] temp = tempName.split(",");
+                	int tempLen = temp.length;
+                	String sIp = temp[0];
+                	String dIp = temp[tempLen-1];
+                	String range = sIp + "_" + dIp;
+                	
+                    //pathname.add(tempName);
+                    //pathNor.add(pathDataItems.get(tempName));//每条路径一個週期内的dataitem
+                    numfirstPeriod = firstPeriod.get(tempName);//周期值string
+                    
+                    for(int j=0;j<alltaskCombination.size();j++){
+                    	TaskCombination tc = alltaskCombination.get(j);
+                    	if(tc.getRange().equals(range)&&tc.getTasks().contains(task)){
+                    		taskCombination = tc;
+                    	}
+                    }
+                    
+                    //解析taskCombination的DataItems格式
+                    List<List<String>> seqs=PMDetect(taskCombination.getDataItems(), task);
+                    List<String> seq = null;
+                    for(int k=0;k<seqs.size();k++){
+                    	List<String> ls = seqs.get(k);
+                    	if(ls.get(0).equals(tempName)){
+                    		seq = ls;
+                    	}
+                    }
+                    DataItems newItems = readSeq(seq, taskCombination.getDataItems());
+                    
+                    
+                    //pathname.add(tempName);
+                	//pathNor.add(taskCombination.getDataItems());
                     pathname.add(tempName);
-                    pathNor.add(pathDataItems.get(tempName));
-                    numfirstPeriod = firstPeriod.get(tempName);
-//                	if (i==oneSeries.size()-1) { //限制一张表中最多3条周期相同的路径
-//                        DataItems nor=new DataItems();
-//                        DataItems abnor=new DataItems();
-//                        JFreeChart jf = ChartPanelShowPP.createChart(pathNor,pathname, nor, abnor,numPeriod,numfirstPeriod);
-//                        ChartPanel chartpanel = new ChartPanel(jf);
-//                        jp.add(chartpanel);
-//
-//                        pathNor = new ArrayList<>();
-//                        pathname = new ArrayList<>();
-//                    }                 
+                	pathNor.add(newItems);
+                    DataItems nor=new DataItems();
+                    DataItems abnor=new DataItems();
+                    
+                    JFreeChart jf = ChartPanelShowPP.createChart(pathNor,pathname, nor, abnor,numPeriod,numfirstPeriod);
+                    ChartPanel chartpanel = new ChartPanel(jf);
+                    jp.add(chartpanel);
+                    pathNor = new ArrayList<>();
+                    pathname = new ArrayList<>();
                 }
-                DataItems nor=new DataItems();
-                DataItems abnor=new DataItems();
-                JFreeChart jf = ChartPanelShowPP.createChart(pathNor,pathname, nor, abnor,numPeriod,numfirstPeriod);
-                ChartPanel chartpanel = new ChartPanel(jf);
-                jp.add(chartpanel);
             }
             repaint();
             validate();
@@ -440,6 +477,63 @@ public class PanelShowResultsPP extends JPanel implements IPanelShowResults {
             count++;
         }
     }
+    /**@author LYH
+     * 解析数据**/
+    public List<List<String>> PMDetect(DataItems dataItems,TaskElement tasks){		
+			
+			List datas = new ArrayList();
+			if (task.getMiningObject().equals("流量")){
+				datas = dataItems.getNonNumData();
+			} else if (task.getMiningObject().equals("通信次数")) {
+				DataPretreatment.translateProbilityOfData(dataItems);//将跳转概率保存到文件中
+				dataItems = DataPretreatment.changeDataToProb(dataItems); //计算每条路径的概率
+				datas = dataItems.getProbMap();
+			}
+			
+			Set<String>varset=dataItems.getVarSet();
+			List<List<String>> seqs=new ArrayList<List<String>>();
+			for(String item:varset){
+				int row=0;
+				List<String>seq=new ArrayList<String>();
+				seq.add(item);
+				
+				Iterator iter = datas.iterator();
+				while(iter.hasNext()){
+					Map map = (Map) iter.next();
+					if(map.containsKey(item)) {
+						if(map.get(item) instanceof Double){	//用于区别Double路径概率与Integer流量
+							int value=(int)((double)map.get(item)*1000);
+							seq.add(value+"");
+							row++;
+						}else if(map.get(item) instanceof Integer){
+							int value = (int) map.get(item);
+							seq.add(value+"");
+							row++;
+						}
+					}else{
+						seq.add("0");
+					}
+				}
+				
+				if(row<dataItems.getLength()*0.05) //如果包含路径item的行很少，则不计入seqs
+					continue;
+				seqs.add(seq);
+			}
+			return seqs;
+    }
+    public DataItems readSeq(List<String> seq,DataItems dataItems){
+    	
+			
+			DataItems newItem=new DataItems();
+			String name=seq.get(0);
+			seq.remove(0);
+			newItem.setData(seq);
+			newItem.setTime(dataItems.getTime());
+			
+			return newItem;
+			
+    }
+    	
 }
 
 
