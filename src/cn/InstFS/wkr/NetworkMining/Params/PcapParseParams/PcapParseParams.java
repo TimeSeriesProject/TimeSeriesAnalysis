04f518115498fc233d44748dde545d66aa862ad5
chenwei <277351771@qq.com>
2016-08-11 18:41:13 +0800

    pcap解析参数构造函数

--- src/cn/InstFS/wkr/NetworkMining/Params/PcapParseParams/PcapParseParams.java
+++ src/cn/InstFS/wkr/NetworkMining/Params/PcapParseParams/PcapParseParams.java
@@ -1,10 +1,16 @@
 package cn.InstFS.wkr.NetworkMining.Params.PcapParseParams;
 
+import org.jdom.Element;
+
 public class PcapParseParams {
 	
 	private String pcapPath;
 	private String outputPath;
-	
+	public PcapParseParams(Element element)
+	{
+		pcapPath= element.getChildText("pcapPath");
+		outputPath= element.getChildText("outputPath");
+	}
 	
 	public String getPcapPath() {
 		return pcapPath;
