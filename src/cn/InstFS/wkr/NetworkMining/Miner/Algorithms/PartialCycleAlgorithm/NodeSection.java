21d879e0600347694878504484051a604ef29ea2
ChenWS <1041469414@qq.com>
2016-10-18 18:59:24 +0800

    NodeSection

---
+++ src/cn/InstFS/wkr/NetworkMining/Miner/Algorithms/PartialCycleAlgorithm/NodeSection.java
@@ -0,0 +1,10 @@
+package cn.InstFS.wkr.NetworkMining.Miner.Algorithms.PartialCycleAlgorithm;
+
+public class NodeSection {
+	public int begin;
+	public int end;
+	public NodeSection(int begin,int end){
+		this.begin=begin;
+		this.end=end;
+	}
+}
