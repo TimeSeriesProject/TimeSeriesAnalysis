fc9d738061487839a55f36da47376957b8a8cb5e
arbor <vlinyq@gmail.com>
2017-03-02 16:50:15 +0800

    路径全无周期时显示

--- src/cn/InstFS/wkr/NetworkMining/ResultDisplay/UI/PanelShowResultsPP.java
+++ src/cn/InstFS/wkr/NetworkMining/ResultDisplay/UI/PanelShowResultsPP.java
@@ -115,6 +115,17 @@ public class PanelShowResultsPP extends JPanel implements IPanelShowResults {
 
             yName=obName;
 
+            if (!rslt.getRetPath().getHasPeriod()) {
+                JLabel text = new JLabel("所有路径均无周期性",Label.CENTER);
+                panel.add(text,BorderLayout.CENTER);
+                jsp.setViewportView(panel);
+                add(jsp);
+
+                repaint();
+                validate();
+                count++;
+                return;
+            }
 
             for (Map.Entry<String, MinerResultsPM> entry : resultPM.entrySet()) {
                 String pathname = entry.getKey();
